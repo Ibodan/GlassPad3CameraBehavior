68240c5419ddfaf7522f8cece03f215ec3713094
Ibodan <Ibodan@gmail.com>
2020-04-19 11:04:34 +0900

    Add project files.

---
+++ GlassPad3CameraBehavior/Plugin.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using IPA;
+using IPALogger = IPA.Logging.Logger;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace GlassPad3CameraBehavior
+{
+	[Plugin(RuntimeOptions.SingleStartInit)]
+	internal class Plugin
+    {
+		private class FrontCameraBehavior : MonoBehaviour
+		{
+			public CustomAvatar.Tracking.AvatarInput input { get; set; }
+
+			private Queue<float> queue = new Queue<float>(128);
+
+			private int enqueueInterval = 4;
+
+			private int enqueueCounter;
+
+			private float averageQueue;
+
+			private void Start()
+			{
+				logger.Debug("behavior start");
+				queue.Enqueue(0f);
+			}
+
+			private void Update()
+			{
+				Pose headPose;
+				if (input.TryGetHeadPose(out headPose) == false) return;
+
+				float y = headPose.position.y;
+				if (enqueueCounter++ >= enqueueInterval)
+				{
+					queue.Enqueue(y);
+					while (queue.Count > 100)
+					{
+						queue.Dequeue();
+					}
+					enqueueCounter = 0;
+					float num = 0f;
+					for (int i = 0; i < queue.Count; i++)
+					{
+						num += queue.ElementAt(i) * ((float)Math.Sin(Math.PI * 2.0 * (double)((float)i / (float)queue.Count) - 0.25) * 0.28f + 1f);
+					}
+					averageQueue = num / (float)queue.Count;
+				}
+				Vector3 position = transform.position;
+				position.y = averageQueue * 0.92f;
+				transform.position = position;
+			}
+		}
+
+		public static IPALogger logger { get; private set; }
+
+		[Init]
+		public Plugin(IPALogger logger)
+		{
+			Plugin.logger = logger;
+		}
+
+		[OnStart]
+		public void OnStart()
+		{
+			SceneManager.activeSceneChanged += OnActiveSceneChanged;
+		}
+
+		public void OnActiveSceneChanged(Scene from, Scene to)
+		{
+			SharedCoroutineStarter.instance.StartCoroutine(InjectBehavior());
+		}
+
+		private IEnumerator InjectBehavior()
+		{
+			yield return new WaitForSecondsRealtime(1.0f);
+
+			Camera camera = Camera.allCameras.FirstOrDefault((Camera c) => c.name == "CPCameraFront" && c.gameObject.layer == 5);
+			if (camera == null) yield break;
+
+			logger.Debug("camera found");
+
+			System.Object avatar = ReflectionUtil.GetPrivateProperty<System.Object>(CustomAvatar.AvatarManager.instance, "currentlySpawnedAvatar");
+			if (avatar == null) yield break;
+
+			logger.Debug("spawned avatar found");
+
+			System.Object tracking = ReflectionUtil.GetPrivateProperty<System.Object>(avatar, "tracking");
+			if (tracking == null) yield break;
+
+			logger.Debug("tracking found");
+
+			CustomAvatar.Tracking.AvatarInput input = ReflectionUtil.GetPrivateField<CustomAvatar.Tracking.AvatarInput>(tracking, "input");
+			if (input == null) yield break;
+
+			logger.Debug("input found");
+			logger.Debug("setting up behavior");
+
+			FrontCameraBehavior behavior = camera.GetComponent<FrontCameraBehavior>();
+			if (behavior == null)
+			{
+				behavior = camera.gameObject.AddComponent<FrontCameraBehavior>();
+			}
+			behavior.input = input;
+
+			logger.Info("ready to go");
+		}
+
+	}
+}
---
+++ GlassPad3CameraBehavior/ReflectionUtil.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Reflection;
+using UnityEngine;
+
+internal static class ReflectionUtil
+{
+	public static void SetPrivateField(object obj, string fieldName, object value)
+	{
+		obj.GetType().GetField(fieldName, BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic).SetValue(obj, value);
+	}
+
+	public static T GetPrivateField<T>(object obj, string fieldName)
+	{
+		return (T)obj.GetType().GetField(fieldName, BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic).GetValue(obj);
+	}
+
+	public static void SetPrivateProperty(object obj, string propertyName, object value)
+	{
+		obj.GetType().GetProperty(propertyName, BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic).SetValue(obj, value, null);
+	}
+
+	public static T GetPrivateProperty<T>(object obj, string propertyName)
+	{
+		return (T)obj.GetType().GetProperty(propertyName, BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic).GetValue(obj);
+	}
+
+	public static void InvokePrivateMethod(object obj, string methodName, object[] methodParams)
+	{
+		obj.GetType().GetMethod(methodName, BindingFlags.Instance | BindingFlags.NonPublic).Invoke(obj, methodParams);
+	}
+
+	public static Component CopyComponent(Component original, Type overridingType, GameObject destination)
+	{
+		Component val = destination.AddComponent(overridingType);
+		FieldInfo[] fields = ((object)original).GetType().GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.GetField);
+		foreach (FieldInfo fieldInfo in fields)
+		{
+			fieldInfo.SetValue(val, fieldInfo.GetValue(original));
+		}
+		return val;
+	}
+}
